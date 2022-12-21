0ff37f78275a3154ae3ce86a530aa472c24ec404
Matheus <matheus-rodrigues37@live.com>
2022-12-21 00:13:14 -0300

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,55 +3,22 @@ import Link from 'next/link';
 
 export default function Index() {
 	return (
-		<div className="px-6 md:px-0 flex flex-col items-center justify-center h-screen max-w-2xl gap-10 mx-auto text-center">
-			<motion.h1
-				initial={{
-					opacity: 0,
-					translateX: -100
-				}}
-				animate={{
-					opacity: 1,
-					translateX: 0
-				}}
-				transition={{
-					duration: 1
-				}}
-				className="text-6xl font-bold">
-				Hi, how are you?
-			</motion.h1>
-			<motion.p
-				initial={{
-					opacity: 0,
-					translateX: 100
-				}}
-				animate={{
-					opacity: 1,
-					translateX: 0
-				}}
-				transition={{
-					duration: 1
-				}}
-				className="text-xl ">
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 1 }}
+			className="flex flex-col items-center justify-center h-screen max-w-2xl gap-10 px-6 mx-auto text-center md:px-0">
+			<h1 className="text-6xl font-bold">Hi, how are you?</h1>
+			<p className="text-xl ">
 				This is a question website, you will answer 10 questions of a varieties of subjects and see if
 				your knolegde is on point
-			</motion.p>
+			</p>
 			<Link href="/questions" className="transition-transform active:scale-95 ">
-				<motion.button
-					initial={{
-						opacity: 0,
-						translateX: -100
-					}}
-					animate={{
-						opacity: 1,
-						translateX: 0
-					}}
-					transition={{
-						duration: 1
-					}}
-					className="p-4 font-semibold rounded bg-slate-600 text-slate-300">
+				<button className="p-4 font-semibold rounded bg-slate-600 text-slate-300">
 					Click here to start and good luck!
-				</motion.button>
+				</button>
 			</Link>
-		</div>
+		</motion.div>
 	);
 }
